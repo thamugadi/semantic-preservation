c4c008702e80f695f18807a606814aca549385fc
aramya <22577625+thamugadi@users.noreply.github.com>
2023-10-10 05:19:40 +0100

    inc, dec

--- src/Common.v
+++ src/Common.v
@@ -1,3 +1,6 @@
+Require Import List.
+Import ListNotations.
+
 Module Common.
 
 Definition embed {A : Type} {B : Type} (f : A -> B) :=
@@ -19,5 +22,19 @@ Qed.
 Inductive plus {A : Type} (R : A -> A -> Prop) : A -> A -> Prop :=
   | t_base : forall x y, R x y -> plus R x y
   | t_trans : forall x y z, R x y -> plus R y z -> plus R x z.
+
+Fixpoint take {A : Type} (n : nat) (l : list A) : list A :=
+  match n, l with
+  | 0, _ => []
+  | _, [] => []
+  | S n', h::t => h :: take n' t
+  end.
+
+Fixpoint drop {A : Type} (n : nat) (l : list A) : list A :=
+  match n, l with
+  | 0, _ => l
+  | _, [] => []
+  | S n', h::t => drop n' t
+  end.
 
 End Common.
--- src/Language.v
+++ src/Language.v
@@ -1,7 +1,10 @@
 Module Language.
 Require Import List.
 Import ListNotations.
-
+Require Import PeanoNat.
+Require Import Common.
+Require Import Lia.
+Import Nat.
 Inductive lang_instr : Type :=
   | PtrInc : lang_instr
   | PtrDec : lang_instr
@@ -30,6 +33,20 @@ Fixpoint read_instr' (prog : lang_program) (pc : nat) : lang_instr :=
 Inductive read_instr (p : lang_state) (i : lang_instr) : Prop  :=
   | ri : read_instr' p.(prog) p.(pc) = i -> read_instr p i.
 
+Inductive mem_diff_p (m : list nat) (m' : list nat) (ptr : nat) : Prop :=
+  | c_diff_p : Common.take ptr m = Common.take ptr m' ->
+               Common.drop (ptr+1) m = Common.drop (ptr+1) m' ->
+               Common.drop (ptr+1) (Common.take ptr m') = (map (fun x => x + 1))
+               (Common.drop (ptr+1) (Common.take ptr m)) 
+               -> mem_diff_p m m' ptr.
+
+Inductive mem_diff_m (m : list nat) (m' : list nat) (ptr : nat) : Prop :=
+  | c_diff_m : Common.take ptr m = Common.take ptr m' ->
+               Common.drop (ptr+1) m = Common.drop (ptr+1) m' ->
+               Common.drop (ptr+1) (Common.take ptr m') = (map (fun x => x - 1))
+               (Common.drop (ptr+1) (Common.take ptr m)) 
+               -> mem_diff_m m m' ptr.
+
 (* Small-step operational semantics for our source language.*)
 Inductive lang_semantics (p : lang_state) (p' : lang_state) : Prop :=
   | lang_ptr_inc : read_instr p PtrInc -> p.(ptr) + 1 = p'.(ptr) ->
@@ -37,6 +54,12 @@ Inductive lang_semantics (p : lang_state) (p' : lang_state) : Prop :=
                    p.(mem) = p'.(mem) -> lang_semantics p p' 
   | lang_ptr_dec : read_instr p PtrDec -> p.(ptr) - 1 = p'.(ptr) ->
                    p.(pc) + 1 = p'.(pc) -> p.(prog) = p'.(prog) ->
-                   p.(mem) = p'.(mem) -> lang_semantics p p'.
-
+                   p.(mem) = p'.(mem) -> lang_semantics p p'
+  | lang_inc : read_instr p Inc -> length p.(mem) > p.(ptr) ->
+               p.(pc) + 1 = p'.(pc) -> p.(prog) = p'.(prog) ->
+               mem_diff_p p.(mem) p'.(mem) p.(ptr) -> lang_semantics p p' 
+  | lang_dec : read_instr p Dec -> length p.(mem) > p.(ptr) ->
+               p.(pc) + 1 = p'.(pc) -> p.(prog) = p'.(prog) ->
+               mem_diff_m p.(mem) p'.(mem) p.(ptr) -> lang_semantics p p'. 
+  
 End Language.
